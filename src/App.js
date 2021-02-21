d54c3cf164dcadac59c3c8aef61882e6aae04538
Khimish009 <salman.khimishev01@gmail.com>
2021-02-21 17:15:19 +0300

    installing dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Header from './components/Header';
+import Summary from './components/Summary';
+import Detailed from './components/Detailed';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      Hello chartjs
-    </div>
+    <Container maxWidth="md">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Summary} />
+          <Route path="/detailed" component={Detailed} />
+          <Route path="*"><Redirect to="/" /></Route>
+        </Switch>
+      </Router>
+    </Container>
   );
 }
 
